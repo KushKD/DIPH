7b8f191c49ea8e9b644f71452f5f6e9c860585b5
Kush Dhawan <kushkumardhawan@gmail.com>
2020-06-29 22:50:58 +0530

    Changes Made in QR Code

--- src/main/java/com/dit/himachal/utilities/GeneratePdfReport.java
+++ src/main/java/com/dit/himachal/utilities/GeneratePdfReport.java
@@ -27,7 +27,7 @@ public class GeneratePdfReport {
         vehicleOwnerEntries = data;
         String postJson = objectMapper.writeValueAsString(vehicleOwnerEntries);
         System.out.println("43434" + postJson);
-        Document document = new Document(new Rectangle(200f, 500f).rotate(), 5f, 5f, 5f, 5f);
+        Document document = new Document(new Rectangle(400f, 500f).rotate(), 5f, 5f, 5f, 5f);
         document.addTitle(vehicleOwnerEntries.getIdCardNumber());
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -39,129 +39,123 @@ public class GeneratePdfReport {
             table.setWidthPercentage(100);
             table.setSpacingBefore(10f);
             table.setSpacingAfter(10f);
-            float[] columnWidths = {1f, 1f,2f};
+
+            float[] columnWidths = {1f, 1f, 1f, 1f};
             table.setWidths(columnWidths);
 
 
             PdfPCell cell;
 
-            cell = new PdfPCell();
-            cell.setColspan(2);
+
+            cell = new PdfPCell(new Phrase("Shimla District Police"));
+            cell.setColspan(1);
             cell.setRowspan(2);
+
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
             table.addCell(cell);
 
 
             cell = new PdfPCell(new Phrase("Shimla District Police"));
-            cell.setColspan(2);
+            cell.setColspan(3);
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(vehicleOwnerEntries.getIdCardNumber()));
+            cell.setColspan(3);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
             table.addCell(cell);
 
             cell = new PdfPCell(new Phrase("ID Card for Apple Season 2020 only"));
-            cell.setColspan(2);
+            cell.setColspan(4);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
             table.addCell(cell);
 
 
-            cell = new PdfPCell(new Phrase(vehicleOwnerEntries.getIdCardNumber()));
-            cell.setColspan(2);
+            // Create a new Table
+            PdfPTable childTable1 = new PdfPTable(2);
+            float[] columnWidthsnested = {150f, 150f};
+            childTable1.setWidths(columnWidthsnested);
+            childTable1.addCell("Name:");
+            childTable1.addCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerName()));
+            childTable1.addCell("Chassis Number:");
+            childTable1.addCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerChassisNumber()));
+            childTable1.addCell("Driving Licence Number:");
+            childTable1.addCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerDrivingLicence()));
+            childTable1.addCell("Engine Number:");
+            childTable1.addCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerEngineNumber()));
+            childTable1.addCell("Chassis Number:");
+            childTable1.addCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerChassisNumber()));
+            childTable1.addCell("Mobile Number:");
+            childTable1.addCell(new Phrase(String.valueOf(vehicleOwnerEntries.getVehicleOwnerMobileNumber())));
+            childTable1.addCell("Valid Upto:");
+            childTable1.addCell(new Phrase(vehicleOwnerEntries.getIsValidUpto()));
+            // Add the new table to the Cell of parent table
+            // table.addCell(childTable1);
+
+            cell = new PdfPCell(childTable1);
+            cell.setColspan(3);
+            cell.setBorder(0);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
             table.addCell(cell);
 
-            cell = new PdfPCell(new Phrase("Name:-"));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerName()));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase("Chassis Number:-"));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerChassisNumber()));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase("Driving Licence Number:-"));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerDrivingLicence()));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase("Engine Number:-"));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase(vehicleOwnerEntries.getVehicleOwnerEngineNumber()));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase("Mobile Number:-"));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase(String.valueOf(vehicleOwnerEntries.getVehicleOwnerMobileNumber())));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase("Valid Upto:-"));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase(vehicleOwnerEntries.getIsValidUpto()));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-
-
-            BarcodeQRCode barcodeQRCode = new BarcodeQRCode(postJson, 1000, 1000, null);
+            //postJson
+            BarcodeQRCode barcodeQRCode = new BarcodeQRCode(postJson, 100, 100, null);
             Image codeQrImage = barcodeQRCode.getImage();
-            codeQrImage.scaleAbsolute(50, 50);
+            codeQrImage.setAbsolutePosition(50f, 50f);
+            //codeQrImage.scalePercent(10, 10);
 
-            cell = new PdfPCell();
+
+           // Image image = Image.getInstance(new URL(Utilities.getPhotoUrl(vehicleOwnerEntries.getVehicleOwnerImageName())));
+
+
+
+            // Create a new Table
+            PdfPTable childTable2 = new PdfPTable(1);
+
+            float[] columnWidthsnested2 = {10f};
+            childTable2.setWidths(columnWidthsnested2);
+
+              childTable2.addCell(codeQrImage);
+            childTable2.getDefaultCell().setBorder(0);
+           // childTable2.addCell(image);
+
+
+            cell = new PdfPCell(childTable2);
+            cell.setColspan(1);
+            cell.setBorder(0);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(codeQrImage);
+            table.addCell(cell);
+//
 
+//
+//            cell = new PdfPCell(codeQrImage);
+//            cell.setColspan(2);
+//            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+//            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+//            table.addCell(cell);
 
-
-
-            Image image = Image.getInstance(new URL(Utilities.getPhotoUrl(vehicleOwnerEntries.getVehicleOwnerImageName())));
 
             PdfWriter.getInstance(document, out);
             document.open();
 
             document.add(table);
-            document.add(image);
+            //    document.add(image);
 
             document.close();
 
-        } catch (DocumentException | MalformedURLException ex) {
+        } catch (DocumentException ex) {  // | MalformedURLException ex
 
             Logger.getLogger(GeneratePdfReport.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         return new ByteArrayInputStream(out.toByteArray());
     }
